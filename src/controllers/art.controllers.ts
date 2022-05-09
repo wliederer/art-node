3794a009df6a397d4ca1cccddd85883c1f010ecc
wliederer <williamliederer@gmail.com>
2022-05-09 17:10:18 -0500

    return signed urls

--- src/controllers/art.controllers.ts
+++ src/controllers/art.controllers.ts
@@ -4,13 +4,9 @@ import { getBucket } from '../services/art.services';
 
 export const artAws = async (req:Request,res:Response) => {
     try{
-        const art = await getBucket();
-        if(art){
-            return res.status(200).send(art)
-        }
-        else {
-            return res.status(200).send('no art')
-        }
+        return await getBucket().then(data=>{Promise.all(data).then(stuff=>{return res.send(stuff)})});
+                
+        
     }catch(err){
         console.error(err)
         return res.status(500)
--- src/services/art.services.ts
+++ src/services/art.services.ts
@@ -13,15 +13,16 @@ AWS.config.update({...credentials,region:'us-east-2'})
 const s3 = new AWS.S3({});
 
 export const getBucket = async():Promise<any> => {
-   const object = s3.getObject({Bucket:String(bucket),Key:"art/orange.jpeg"}).promise();
+  let signedUrls;
+  //  const object = s3.getObject({Bucket:'billsart',Key:"art/orange.jpeg"}).promise();
     const objectUrl = s3.getSignedUrlPromise('getObject',{Bucket:String(bucket),Key:"art/orange.jpeg"});
-  const images =  s3.listObjects({Bucket:String(bucket)}).promise();
+  const images =  s3.listObjects({Bucket:'billsart'}).promise();
 
-  await objectUrl.then(data =>{
-      // console.log(data)
-  }).catch(err=>{
-      throw err
-  })
+  // await objectUrl.then(data =>{
+  //     // console.log(data)
+  // }).catch(err=>{
+  //     throw err
+  // })
 
   // await object.then(data=>{
   //     console.log(data)
@@ -29,12 +30,27 @@ export const getBucket = async():Promise<any> => {
   //     throw err;
   // });
 
-  return await images.then((data)=>{
-      console.log(data)
-      return data;
+  const awsObjects = await images.then(async (data:S3.ListObjectsOutput)=>{
+    return data;
   }).catch(err=>{
-      console.log(err)
-      throw err;
+    console.log(err)
+    throw err;
   })
-   
+  if(awsObjects.Contents){
+   signedUrls = awsObjects.Contents?.map(async(d,i) =>{
+      if(i!= 0){
+        // const url = await getUrl(String(d.Key))
+      
+        // return url
+        return await s3.getSignedUrlPromise('getObject',{Bucket:String(bucket),Key:d.Key})
+
+      } 
+
+    })
+  }
+  return signedUrls
+}
+
+const getUrl = async(key:string)=>{
+  return s3.getSignedUrl('getObject',{Bucket:String(bucket),Key:key})
 }
