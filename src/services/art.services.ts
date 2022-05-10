25667291a158b5bd9a9356ec8ae124352da2b0f8
wliederer <williamliederer@gmail.com>
2022-05-09 19:50:44 -0500

    update art service

--- src/services/art.services.ts
+++ src/services/art.services.ts
@@ -14,21 +14,7 @@ const s3 = new AWS.S3({});
 
 export const getBucket = async():Promise<any> => {
   let signedUrls;
-  //  const object = s3.getObject({Bucket:'billsart',Key:"art/orange.jpeg"}).promise();
-    const objectUrl = s3.getSignedUrlPromise('getObject',{Bucket:String(bucket),Key:"art/orange.jpeg"});
   const images =  s3.listObjects({Bucket:'billsart'}).promise();
-
-  // await objectUrl.then(data =>{
-  //     // console.log(data)
-  // }).catch(err=>{
-  //     throw err
-  // })
-
-  // await object.then(data=>{
-  //     console.log(data)
-  // }).catch(err=>{
-  //     throw err;
-  // });
 
   const awsObjects = await images.then(async (data:S3.ListObjectsOutput)=>{
     return data;
@@ -39,9 +25,7 @@ export const getBucket = async():Promise<any> => {
   if(awsObjects.Contents){
    signedUrls = awsObjects.Contents?.map(async(d,i) =>{
       if(i!= 0){
-        // const url = await getUrl(String(d.Key))
-      
-        // return url
+    
         return await s3.getSignedUrlPromise('getObject',{Bucket:String(bucket),Key:d.Key})
 
       } 
