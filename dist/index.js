96c0a6089ff9b2fdb56b027093931d05db948754
wliederer <williamliederer@gmail.com>
2022-05-11 07:28:14 -0500

    add nodemon

--- dist/index.js
+++ dist/index.js
@@ -7,16 +7,19 @@ require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const art_routes_1 = __importDefault(require("./routes/art.routes"));
 const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
 const corsOptions_1 = require("./constants/corsOptions");
 const app = (0, express_1.default)();
 const port = 8080;
+app.use((0, morgan_1.default)("combined"));
 app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)());
 app.use(art_routes_1.default);
 app.get("/health", (0, cors_1.default)(corsOptions_1.publicCorsConfig), (req, res) => {
-    res.send('ok');
+    res.send("ok");
 });
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
-import 'dotenv/config'
-import express,{Response,Request} from 'express'
-import artRouter from './routes/art.routes';
-import helmet from 'helmet';
-import cors from 'cors'
-import { publicCorsConfig } from './constants/corsOptions';
+import "dotenv/config";
+import express, { Response, Request } from "express";
+import artRouter from "./routes/art.routes";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import { publicCorsConfig } from "./constants/corsOptions";
 
 const app = express();
 const port = 8080;
+app.use(morgan("combined"));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use(helmet())
-app.use(express.json())
-app.use(cors())
+app.use(artRouter);
 
-app.use(artRouter)
+app.get("/health", cors(publicCorsConfig), (req: Request, res: Response) => {
+  res.send("ok");
+});
 
-app.get("/health",cors(publicCorsConfig),(req:Request,res:Response)=>{
-  res.send('ok')
-})
-
-app.listen(port,()=>{
-  console.log(`listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
